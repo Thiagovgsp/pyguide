a663c668e852209fba5281e2dddd7f8e9169a0cf
Thiagovgsp <thiagovgsp@gmail.com>
2024-03-01 13:44:14 -0300

    loop update

---
+++ Looping through strings.py
@@ -0,0 +1,13 @@
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter) #starts counting from 0
+    index = index + 1
+
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index + 1, letter) #starts counting from 1
+    index = index + 1
